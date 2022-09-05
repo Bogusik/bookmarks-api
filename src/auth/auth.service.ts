a2d56e39b5cca187eea5fa61016aeebf783f73f2
Bohuslav Semenov <semenov0310@gmail.com>
2022-09-05 22:37:33 +0200

    Prisma and dto setup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return 'I am login.';
+  constructor(private db: PrismaService) {}
+
+  login(dto: LoginDto) {
+    return dto;
   }
 
   signup() {
